fb3566445246643bd67af95a5d45febb4d0bbf3f
Eric Wang <ewang1997@gmail.com>
2017-03-25 20:24:15 -0400

    prettify

--- extension/script.js
+++ extension/script.js
@@ -1,6 +1,6 @@
 $.get("http://motivating-tabs.herokuapp.com/", null, function(data) {
   var obj = JSON.parse(data);
 
-  $('#author').html(obj.author);
-  $('#quote').html(obj.quote);
+  $('#quote').html('"'+obj.quote+'"');
+  $('#author').html('-'+obj.author);
 });
